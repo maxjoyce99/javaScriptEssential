0c5123cfa84c18a3b274c77639a65f2bd796467f
maxjoyce99 <maxjoyce99@gmail.com>
2024-04-11 16:38:34 -0400

    Added user experience field

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -9,6 +9,7 @@ const job = document.getElementById('job').value;
 const designation = document.getElementById('designation').value;
 const productType = document.getElementById('productType').value;
 const feedback = document.getElementById('feedbackText').value;
+const userExperienceText = document.getElementById('userExperienceText').value;
     alert('Thank you for your valuable feedback');
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -17,6 +18,7 @@ const feedback = document.getElementById('feedbackText').value;
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = userExperienceText;
     document.getElementById('userInfo').style.display = 'block';
 }
 
